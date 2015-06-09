d94bc6860e139ae0b01bed3fd8f0f60c06db4393
Mike <mike.modzelewski@gmail.com>
2015-06-09 22:03:37 +0200

    database console client

---
+++ FileStorage/Client.ConsoleDatabaseClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ConsoleDatabaseClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString =
+                ConfigurationManager.ConnectionStrings["FileStorageDatabase"].ConnectionString;
+        }
+    }
+}
