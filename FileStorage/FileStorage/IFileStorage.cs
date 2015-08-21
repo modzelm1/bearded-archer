a7f60e2c645ff584bc30be4729dd18fe3ba0ce6f
Mike <mike.modzelewski@gmail.com>
2015-08-21 21:33:53 +0200

    niew items

---
+++ FileStorage/FileStorage/IFileStorage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileStorage
+{
+    interface IFileStorage
+    {
+        FileEnvelope GetFileById(Guid fileId);
+
+        void AddFile(FileEnvelope file);
+    }
+}
